5e00d1b83fc776ee625bfa6a33a7cbb0a6cc9031
Shashank Shekhar Singh <125957195+ShashankSingh9@users.noreply.github.com>
2023-02-28 11:15:18 +0530

    Working with JSX  completed

---
+++ WorkingwithJSX.js
@@ -0,0 +1,21 @@
+
+// Exercise: Working with JSX Code
+// This is a quick exercise to get started with React and JSX code: Your task is to edit the JSX code of a provided React component such that it displays a h1 element with the text "Exercise done!" on the page.
+
+// Important: The custom JSX code must be added inside the existing <div> element.
+
+
+
+
+
+import React from 'react';
+
+// don't change the Component name "App"
+export default function App() {
+    return (
+        <div>
+        <h1>Exercise done!</h1>
+        <p>Practicing React...</p>
+        </div>
+    );
+}
