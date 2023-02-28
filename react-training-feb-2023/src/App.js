2274bcdc1218b575ea9caaa5fcf2d446d18794b7
shashankshekhar singh <Shashank.Singh9@ibm.com>
2023-02-28 11:36:53 +0530

    Coding Exercise 3 completed

---
+++ react-training-feb-2023/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import ExerciseComponent from './ExerciseComponent.js'
+
+// don't change the Component name "App"
+export default function App() {
+    return <div>
+    <p>Todo: solve exercise</p>
+    <ExerciseComponent/>
+    </div>;
+}
---
+++ react-training-feb-2023/src/components/ExerciseComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ExerciseComponent() {
+  return (
+    <h2>First exercise - done !</h2>
+  )
+}
+
+export default ExerciseComponent
+
